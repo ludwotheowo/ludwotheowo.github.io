c82caf3c06addbe4cdc7cf9aa67f667a095a3f20
ludwotheowo <ludwotheowo@gmail.com>
2024-02-19 00:49:45 +0100

    interactive terminal

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
+$('body').terminal({
+    hello: function(what) {
+        this.echo('Hello, ' + what + '. Wellcome to this terminal.');
+        // this string can be written with ES6 - uncomment to test
+        // this.echo(`Hello, ${what}. Wellcome to this terminal.`);
+    },
+    help: function() {
+        this.echo('Hello');
+        // this string can be written with ES6 - uncomment to test
+        // this.echo(`Hello, ${what}. Wellcome to this terminal.`);
+    }
+}
+
 function lightmode() {
 	var element = document.body;
 	element.classList.toggle("lightmode");
-} 
+}
