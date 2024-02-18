448336a6442f94a998a63a9f5779cdc3f483a6a7
ludwotheowo <ludwotheowo@gmail.com>
2024-02-19 00:09:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-function command(text) {
-	switch(text) {
-		case 'help':
-			help;
-			break;
-		case 'about':
-			about;
-			break;
-	}
-}
-
 function lightmode() {
 	var element = document.body;
 	element.classList.toggle("lightmode");
