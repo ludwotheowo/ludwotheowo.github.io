02f14f60e13d7a6e16b99175068932b1440a57cf
ludwotheowo <ludwotheowo@gmail.com>
2024-02-19 01:00:01 +0100

    reset

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
-$('body').terminal({
-    hello: function(what) {
-        this.echo('Hello, ' + what + '. Wellcome to this terminal.');
-        // this string can be written with ES6 - uncomment to test
-        // this.echo(`Hello, ${what}. Wellcome to this terminal.`);
-    },
-    help: function() {
-        this.echo('Hello');
-        // this string can be written with ES6 - uncomment to test
-        // this.echo(`Hello, ${what}. Wellcome to this terminal.`);
-    },
-	lightmode: function() {
-		var element = document.body;
-		element.classList.toggle("lightmode");
-	}
+function lightmode() {
+	var element = document.body;
+	element.classList.toggle("lightmode");
 }
