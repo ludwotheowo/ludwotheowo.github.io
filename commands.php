02a9c376de52611de1397ce3ca8d50196ebe9c77
ludwotheowo <ludwotheowo@gmail.com>
2024-02-19 14:42:05 +0100

    commands

--- commands.php
+++ commands.php
@@ -1,3 +1,14 @@
 <?php
-
+$command = $_REQUEST["command"];
+switch ($command) {
+    case 'help':
+        echo "Help command text";
+        break;
+    
+    default:
+        echo "Invalid command";
+        break;
+}
+header("location: index.php");
+die();
 ?>
