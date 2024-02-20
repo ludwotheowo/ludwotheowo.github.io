fb07ba9599d2147933e1190fcb7bf736691782c1
ludwotheowo <ludwotheowo@gmail.com>
2024-02-20 18:50:49 +0100

    switch

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ function lightmode() {
 	var element = document.body;
 	element.classList.toggle("lightmode");
 }
+
+function command(text) {
+	switch (text) {
+		case 'help':
+			"Help command";
+			break;
+		default:
+			"Unrecognised command";
+			break;
+	}
+}
